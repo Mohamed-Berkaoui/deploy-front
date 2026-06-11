51dfeb62b3c65b274c3d90d7ca39523c8aaf1dec
mohamed <berkaouimed1@gmail.com>
2026-06-11 21:51:14 +0100

    UDT url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://deploy-backend-0dab.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
